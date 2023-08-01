14f10f2f2573f94173e67ee7109478d337320733
dotruchon <dorian.truchon@epitech.eu>
2023-08-01 10:35:16 +0200

    :sparkles: Added Docker-PaperMC-Server as project files for PaperMC server docker image.

---
+++ Docker-PaperMC-Server/getpaperplugins.sh
@@ -0,0 +1,10 @@
+
+echo "---------------------------
+      Downloading paper plugins...
+      ---------------------------"
+mkdir plugins/
+cd plugins/
+
+wget https://mediafilez.forgecdn.net/files/4632/182/Dynmap-3.6-spigot.jar
+wget https://mediafilez.forgecdn.net/files/4612/590/SilkSpawners.jar
+wget https://mediafilez.forgecdn.net/files/4655/597/voicechat-bukkit-2.4.15.jar
